caa7e426204e7e4b4287d4608475908cff7ff321
BBQwings <35308805+BBQwings@users.noreply.github.com>
2024-05-06 14:14:37 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests 
+import re 
+from bs4 import BeautifulSoup 
+
+# Target url
+PAGE_URL = "https://www.wikipedia.org"
+
+def get_html_of(url): 
+	resp = requests.get(url) 
+# if status code 200 is not returned, return an error
+	if resp.status_code != 200: 
+		print(f'HTTP status code of {resp.status_code} returned, but 200 was expected. Exiting...')
+		exit(1)
+# return the content of the website
+	return resp.content.decode()
+
+def get_all_words_from(url):
+	html = get_html_of(PAGE_URL) 
+	soup = BeautifulSoup(html, 'html.parser')
+	raw_text = soup.get_text() 
+	# r'\w' = interprets it as individual characters, instead of an escaped character
+	return re.findall(r'\w+', raw_text)
+
+# However, this can all be cut down to 2 lines: 
+def count_occurences_in(word_list):
+	word_list = {}
+	for word in all_words: 
+		word_count[word] = word_count.setdefault(word, 0) + 1
+	return word_count
+# Sort the words in a list
+def get_top_words_from(all_words): 
+	occurrences = count_occurences_in(all_words)
+	return sorted(word_count.items(), key=lambda item: item[1], reverse=True)
+
+all_words = get_all_words_from(PAGE_URL)
+top_words = get_top_words
+
+# Print the top 10 words 
+for i in range(10): 
+	print(top_words[i][0])
